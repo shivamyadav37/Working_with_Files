ccab43d3722362b0b71e31b2f79f792c1ec67d2b
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-04-02 19:00:21 +0530

    IDk

--- src/ReadinFromFile.java
+++ src/ReadinFromFile.java
@@ -1,12 +1,16 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ReadinFromFile {
     public static void main(String[] args) throws IOException {
 
         //Reading from the file that we created previously.
         File file = new File("Output.txt");
+        if (file.exists()){
+            Scanner inputFile = new Scanner(file);
 
+        }
 
     }
 }
