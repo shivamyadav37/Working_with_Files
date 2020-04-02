5af9ad1f244b1bb8338eb2622a71de62d5b3cfc7
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-04-02 18:56:52 +0530

    IDk

--- src/ReadinFromFile.java
+++ src/ReadinFromFile.java
@@ -3,7 +3,10 @@ import java.io.IOException;
 
 public class ReadinFromFile {
     public static void main(String[] args) throws IOException {
+
+        //Reading from the file that we created previously.
         File file = new File("Output.txt");
+
 
     }
 }
