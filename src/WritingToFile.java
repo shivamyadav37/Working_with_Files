0bebf6b20b5d423c0b22543130d819b3f5fe47a8
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-04-02 18:47:15 +0530

    IDk

--- src/WritingToFile.java
+++ src/WritingToFile.java
@@ -1,7 +1,12 @@
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 public class WritingToFile {
     public static void main(String[] args) throws IOException {
+        //Create and open the file
+        Writer out;
+        PrintWriter OutPutFile = new PrintWriter("Output.txt");
 
     }
 }
